b3f5d1ba494ffa63702fcfdd824952a80a63d331
Winter <jitd1222@gmail.com>
2023-11-16 21:26:37 +0800

    feat: iframe mode

--- _/script.py
+++ _/script.py
@@ -2,6 +2,7 @@ import os
 import shutil
 
 template = '<script>window.location.href="{}";</script>'
+template_iframe = '<iframe src="{}" frameborder="0" style="width:100%;height:100%;"></iframe>'
 with open('./_/config.txt', encoding='utf-8') as f:
     rules = [r.strip() for r in f.readlines() if r.strip() and not r.startswith('#')]
 
@@ -11,9 +12,13 @@ for dir in dirs:
     shutil.rmtree(dir, ignore_errors=True)
 
 for rule in rules:
-    code, url = rule.split(',')
+    code, url, *rest = rule.split(',')
+    tpl = template
+    if rest and rest[0].strip() == 'iframe':
+        tpl = template_iframe
+
     if os.path.exists(code):
         continue
     os.makedirs(code)
     with open(os.path.join(code, 'index.html'), 'w', encoding='utf-8') as f:
-        f.write(template.format(url))
+        f.write(tpl.format(url))
